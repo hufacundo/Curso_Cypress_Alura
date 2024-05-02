720fa1bd8714ec430e14618ae30af38e83a7ceb5
Hugo Facundo <89493574+hufacundo@users.noreply.github.com>
2024-05-02 17:03:27 -0300

    Add files via upload

---
+++ Curso_Cypress_Alura/cypress/e2e/api-mensagens.cy.js
@@ -0,0 +1,15 @@
+describe('Api Adopet', () => {
+     
+
+    it('Mensagens da API', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://adopet-api-i8qu.onrender.com/mensagem/b9ad66a6-b59a-4c4c-b368-6b840b99e40a',
+            headers: Cypress.env()
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('msg')          
+        })
+    })
+});
---
+++ Curso_Cypress_Alura/cypress/e2e/cadastro-correto.cy.js
@@ -0,0 +1,13 @@
+describe('Página de Cadastro', () => {
+  
+  beforeEach(() => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="register-button"]').click();
+});
+
+  it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () =>{ 
+    
+    cy.cadastro('Hugo do Morro', 'hugo@email.com', 'Senha123', 'Senha123');
+
+  })
+})
